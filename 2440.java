8725024268a33a08ae8df43875fd98c10d2564e1
rave189 <59406960+rave189@users.noreply.github.com>
2020-01-26 18:14:42 +0900

    Create 2440.java

---
+++ 2440.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		for(int i=number; i>0; i--) {
+			for(int j=0; j<i; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+	}
+}
