3f1f93b14b9a6c5137f6af5a804fe8155bbb52cb
rave189 <59406960+rave189@users.noreply.github.com>
2020-01-26 18:11:48 +0900

    Create 1330.java

---
+++ 1330.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int A = s.nextInt();
+		int B = s.nextInt();
+		if (A > B)
+			System.out.println(">");
+		else if (A < B)
+			System.out.println("<");
+		else
+			System.out.println("==");
+	}
+}
