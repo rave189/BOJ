2ed33deef282798aa03ee97b1055aa93ef3ce74e
rave189 <59406960+rave189@users.noreply.github.com>
2020-01-30 23:18:34 +0900

    Create 2742.java

---
+++ 2742.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int number = Integer.parseInt(bf.readLine());
+		for(int i=number; i>0; i--)
+			System.out.println(i);
+	}
+}
