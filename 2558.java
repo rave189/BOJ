d6b86b682a25e765bcc4473d9ce4fd2f3102f7e9
rave189 <59406960+rave189@users.noreply.github.com>
2020-01-30 23:16:53 +0900

    Create 2558.java

---
+++ 2558.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		System.out.println(input.nextInt()+input.nextInt());
+	}
+}
