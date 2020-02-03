1d1a9cac5bb86fbb19148983746fd0a8413280c9
rave189 <59406960+rave189@users.noreply.github.com>
2020-02-03 19:07:42 +0900

    Create 11722.java

---
+++ 11722.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		int[] array = new int[number];
+		for (int i = 0; i < number; i++)
+			array[i] = input.nextInt();
+		int[] result = new int[number];
+		for(int i=0; i<number; i++) {
+			result[i] = 1;
+			for(int j=0; j<i; j++)
+				if(array[i] < array[j] && result[i] < result[j] + 1)
+					result[i] = result[j] + 1;
+		}
+		int max = 0;
+		for(int i=0; i<number; i++)
+				if(result[i] > max)
+					max = result[i];
+		System.out.println(max);
+	}
+}
