f0c610c3ba96ec93175e5fe4d669e17a03f81c8b
rave189 <59406960+rave189@users.noreply.github.com>
2020-05-12 23:13:13 +0900

    Create 15596.java

---
+++ 15596.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        
+    }
+    long sum(int[] a) {
+        long ans = 0;
+        for(int i=0; i< a.length; i++)
+            ans += a[i];
+        return ans;
+    }
+}
