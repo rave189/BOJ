7c4ae6967d6df544c7a7b2a706f33672cb508ad5
rave189 <59406960+rave189@users.noreply.github.com>
2020-07-29 13:57:56 +0900

    Create 11729.java
    하노이탑
    3개의 기둥 중 1번에 있는 원반 n개를 3번으로 이동 시키기
    원반 n-1개를 2번으로 옮긴다 hanoi(n-1, 1, 3, 2) -> 1번에서 3번을 지나 2번으로 옮긴다.
    n번째 원반을 3번으로 옮긴다
    원반 n-1개를 3번으로 옮긴다 hanoi(n-1, 2, 1, 3) -> 2번에서 1번을 지나 3번으로 옮긴다.
    
    출력을 System.out.print와 BufferedWriter를 같이 쓰면 안됨 <- why?
    BufferedWriter만큼 StringBuilder도 빠르기 때문에 StringBuilder를 사용

---
+++ 11729.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static StringBuilder sb = new StringBuilder();
+	static int count = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		hanoi(num, 1, 2, 3);
+		System.out.println(count+"\n"+sb.toString());
+	}
+
+	public static void hanoi(int n, int fir, int sec, int last) throws IOException {
+		if (n == 1) {
+			sb.append(fir + " " + last + "\n");
+			count++;
+		} else {
+			hanoi(n - 1, fir, last, sec);
+			sb.append(fir + " " + last + "\n");
+			count++;
+			hanoi(n - 1, sec, fir, last);
+		}
+	}
+}
