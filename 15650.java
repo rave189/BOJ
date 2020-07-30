7d19e43114b3d246bb21588d6a2117e7ce2b4c73
rave189 <59406960+rave189@users.noreply.github.com>
2020-07-30 17:49:24 +0900

    Create 15650.java
    백트래킹
    15649문제에서 check를 뺀다.
    오름차순 정렬이기 때문에 앞 숫자보다 뒤의 숫자가 크면 되므로 인자로 i+1을 넣어준다.

---
+++ 15650.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int m = input.nextInt();
+		Back(n, 0, m, "", 1);
+	}
+
+	public static void Back(int n, int length, int m, String ans, int num) {
+		if (length <= m)
+			for (int i = 1; i <= n; i++)
+				if (i >= num)
+					Back(n, length + 1, m, ans + i + " ", i+1);
+		if (ans.length() == m * 2)
+			System.out.println(ans);
+	}
+}
