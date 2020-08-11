32aec9162196f710b4e34dd515fa3f55695db065
rave189 <59406960+rave189@users.noreply.github.com>
2020-08-11 16:14:23 +0900

    Update 17626.java
    n을 제곱수의 합으로 표현할 때 가장 적은 개수가 몇 개인지 구하는 문제
    1699번 제곱수의 합과 똑같은 문제이다.
    
    i번째는 i-j*j번째에 j*j를 더해준 경우이다.
    따라서 i번째의 값이 0이거나 i-j*j에 1을 더해준 값보다 작은 경우 값을 업데이트 해준다.

--- 17626.java
+++ 17626.java
@@ -9,16 +9,16 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[++n];
+		int num = Integer.parseInt(br.readLine());
+		int[] arr = new int[++num];
 		arr[1] = 1;
-		for (int i = 1; i < n; i++) {
+		for (int i = 1; i < num; i++) {
 			for (int j = 1; j * j <= i; j++) {
 				int index = i - j * j;
 				if (arr[i] == 0 || arr[index] + 1 < arr[i])
 					arr[i] = arr[index] + 1;
 			}
 		}
-		System.out.println(arr[n - 1]);
+		System.out.println(arr[num - 1]);
 	}
 }
