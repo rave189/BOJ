dfd5bc6a587a17bf880925290c95f6c044e3834d
rave189 <59406960+rave189@users.noreply.github.com>
2020-08-15 19:25:55 +0900

    Create 1697.java
    위치 N에서 위치 K로 가는 경우의 수의 최솟값을 찾는 문제
    N은 N-1, N+1, 2*N으로 이동할 수 있다.
    우선 N-1에서 0까지는 N-i 초기화 한다.
    N+1부터 마지막 인덱스까지는 Integer.MAX_VALUE로 초기화해준다.
    그 후 0부터 마지막 인덱스까지 arr[i*2], arr[i+1], arr[i-1] = arr[i] + 1을 각각의 배열의 값보다 작을 경우만 업데이트 하면서 탐색한다.
    이 때 0부터 순서대로 탐색하기 때문에 2*i-1 에서 2*(2*i-1)이 최소가 되는 경우를 찾지 못한다. (현재 인덱스는 2*i 이기 때문에 이전으로 갈 수 없다.)
    그렇기 때문에 2*i를 업데이트 할 때 2*i-1도 같이 업데이트 해주어야 한다.

---
+++ 1697.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int k = input.nextInt();
+		int[] arr = new int[n + 2 * k + 1];
+		for (int i = n - 1; i >= 0; i--)
+			arr[i] = arr[i + 1] + 1;
+		for (int i = n + 1; i < arr.length; i++)
+			arr[i] = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			if (2 * i < arr.length && arr[i] + 1 < arr[2 * i]) {
+				arr[2 * i] = arr[i] + 1;
+				if (arr[2 * i] + 1 < arr[2 * i - 1])
+					arr[2 * i - 1] = arr[2 * i] + 1;
+			}
+			if (i - 1 >= 0 && arr[i] + 1 < arr[i - 1])
+				arr[i - 1] = arr[i] + 1;
+			if (i + 1 < arr.length && arr[i] + 1 < arr[i + 1])
+				arr[i + 1] = arr[i] + 1;
+		}
+		System.out.println(arr[k]);
+	}
+}
