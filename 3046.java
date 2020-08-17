d6663cfc988f335da4d600adbde6a5a0d736c747
rave189 <59406960+rave189@users.noreply.github.com>
2020-08-17 17:36:11 +0900

    Create 3046.java
    R1과 S를 가지고 R2를 찾는 문제
    (R1+R2)/2 = S 이다.

---
+++ 3046.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println(-1*input.nextInt() + 2*input.nextInt());
+	}
+}
