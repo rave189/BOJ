d447873a029d553af748a25a7fc0774a86e0c49b
rave189 <59406960+rave189@users.noreply.github.com>
2020-08-21 16:55:13 +0900

    Update 10826.java
    10000번 이하의 피보나치 수를 구하는 문제
    수가 매우 커 BigInteger로 계산한다.
    
    아래는 큰 수 연산을 위해 더하기 연산을 String으로 구현한 코드이다.

--- 10826.java
+++ 10826.java
@@ -22,3 +22,49 @@ public class Main {
 		bw.flush();
 	}
 }
+
+/**
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[n+2];
+		arr[0] = "0";
+		arr[1] = "1";
+		for (int i = 2; i <= n; i++)
+			arr[i] = add(arr[i-2], arr[i-1]);
+		bw.write(arr[n]);
+		bw.flush();
+	}
+
+	public static String add(String a, String b) {
+		StringBuilder tmp = new StringBuilder();
+		int carry = 0;
+		for (int i = b.length() - 1, j = a.length() - 1; i >= 0; i--, j--) {
+			int aNum = 0;
+			if (j >= 0)
+				aNum = Integer.parseInt(a.substring(j, j + 1));
+			int bNum = Integer.parseInt(b.substring(i, i + 1));
+			int sum = aNum + bNum + carry;
+			if (sum >= 10) {
+				carry = 1;
+				tmp.append(sum - 10);
+			} else {
+				carry = 0;
+				tmp.append(sum);
+			}
+		}
+		if (carry != 0)
+			tmp.append(carry);
+		return tmp.reverse().toString();
+	}
+}
+*/
