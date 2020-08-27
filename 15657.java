da4cc0e28daf0041fe19840b638f22ff0e4a5a9a
rave189 <59406960+rave189@users.noreply.github.com>
2020-08-27 23:46:31 +0900

    Create 15657.java
    15654번 문제와 비슷한 문제이다.
    수열이 비내림차순이 되도록 출력하는 문제
    항상 이전 값보다 크거나 같은 수만 재귀호출 하도록 한다.

---
+++ 15657.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] arr;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		Back(m, arr[0], "");
+		bw.flush();
+	}
+
+	public static void Back(int depth, int next, String result) throws IOException {
+		if (depth == 0) {
+			bw.write(result + "\n");
+		} else {
+			for (int i = 0; i < arr.length; i++)
+				if (arr[i] >= next)
+					Back(depth - 1, arr[i], result + arr[i] + " ");
+		}
+	}
+}
