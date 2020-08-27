57f64920136a566f6639883e0e4c3e1a96ca93b0
rave189 <59406960+rave189@users.noreply.github.com>
2020-08-27 13:42:08 +0900

    Create 15654.java
    N개의 수를 가진 배열의 수열을 구하는 문제
    깊이가 0이면 그동안의 result를 출력한다.
    깊이가 0이 아니면 배열의 첫 번째 수부터 들어가지 않은 수에서만 재귀호출을 한다.

---
+++ 15654.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		boolean[] check = new boolean[n];
+		Back(arr, check, m, "");
+	}
+
+	public static void Back(int[] arr, boolean[] next, int depth, String result) {
+		if (depth == 0)
+			System.out.println(result);
+		else {
+			for (int i = 0; i < arr.length; i++)
+				if (!next[i]) {
+					next[i] = true;
+					Back(arr, next, depth - 1, result + arr[i] + " ");
+					next[i] = false;
+				}
+		}
+	}
+}
