edf0492a67d199e75d5bb0b2e89fbb1ceb1107f4
rave189 <59406960+rave189@users.noreply.github.com>
2020-09-06 12:36:22 +0900

    Create 2805.java
    1654 랜선 자르기와 비슷한 문제
    n 만큼의 목재를 들고 갈 수 있는 절단기의 최대 높이를 구하는 문제
    min과 max를 두고 이분 탐색으로 절단기의 최대 높이를 구한다.
    절단기보다 높이가 낮은 나무는 잘리지 않으므로 절단기보다 높이가 큰 나무만 나무에서 절단기의 길이를 빼준다.

---
+++ 2805.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int[] arr = new int[k];
+		long max = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < k; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			max = Math.max(arr[i], max);
+		}
+		long maxLength = 0;
+		long min = 1;
+		while (min <= max) {
+			long mid = (min + max) / 2;
+			long count = 0;
+			for (int i = 0; i < k; i++)
+				if (arr[i] > mid)
+					count += arr[i] - mid;
+			if (count >= n) {
+				maxLength = Math.max(maxLength, mid);
+				min = mid + 1;
+			} else
+				max = mid - 1;
+		}
+		bw.write(Long.toString(maxLength));
+		bw.flush();
+	}
+}
