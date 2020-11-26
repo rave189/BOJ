41f9ca3049623659eedf077dc208aa3839be154c
rave189 <59406960+rave189@users.noreply.github.com>
2020-11-26 15:18:25 +0900

    Create 2217.java
    로프 N개가 주어졌을 때 로프가 견딜 수 있는 최대 중량을 구하는 문제
    로프를 전부 사용하지 않아도 되고 로프를 n개 사용한다면 각 로프는 w/n 만큼의 중량을 받는다.
    최대 중량은 로프 n개를 사용했을 때 n개의 로프 중 가장 작은 무게를 견디는 로프 * n을 하면 최대 중량이 나온다.
    따라서 큰 무게를 견디는 로프부터 로프가 견디는 무게 * 현재 로프보다 큰 무게를 견디는 로프의 수를 곱하면 최대 중량이 나온다.

---
+++ 2217.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(br.readLine());
+		Arrays.sort(arr);
+		int max = 0;
+		for (int i = 0; i < n; i++)
+			max = Math.max(max, arr[i] * (n - i));
+		System.out.println(max);
+	}
+}
