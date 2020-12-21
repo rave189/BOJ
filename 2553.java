fbcd95ecb18c233163555942dc4f65866f9f587d
rave189 <59406960+rave189@users.noreply.github.com>
2020-12-21 23:59:48 +0900

    Create 2553.java
    N! 의 0이 아닌 가장 낮은 자리 수를 출력하는 문제
    1부터 N까지 곱셈을 수행하면서 맨 뒤 0과 가장 낮은 자리 수를 제외하고 모두 지워준다.
    곱셈으로 맨 뒤 0을 결정하는 수는 일의 자리 수 혹은 가장 낮은 자리 수 이기 때문에 낮은 자리 수를 제외하고 모두 지워준다.
    여기서 입력이 20000까지이므로 20000보다 큰 수로 나머지를 구해야 올바른 결과 값을 구할 수 있다.
    마지막 출력은 맨뒤 0을 제거하고 일의 자리 수만 출력한다.

---
+++ 2553.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long answer = 1;
+		for (int i = 2; i <= n; i++) {
+			answer *= i;
+			while (answer % 10 == 0)
+				answer /= 10;
+			answer %= 1000000;
+		}
+		while (answer % 10 == 0)
+			answer /= 10;
+		System.out.println(answer % 10);
+	}
+}
