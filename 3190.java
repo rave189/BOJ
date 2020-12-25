9e1b4fcc1c78ca108231a6d01b680f68973300c8
rave189 <59406960+rave189@users.noreply.github.com>
2020-12-25 13:50:54 +0900

    Create 3190.java
    뱀 게임이 종료되는 시간을 출력하는 문제
    이 게임은 벽에 부딪히거나 자기자신과 부딪히면 게임이 종료된다.
    뱀은 머리가 먼저 다음 칸으로 움직인 후 움직인 칸에 사과가 있으면 꼬리는 변하지 않고 사과가 없으면 꼬리를 앞으로 당긴다.
    -> 사과를 먹으면 뱀의 길이가 늘어난다.
    맵의 크기 N이 주어지고 사과가 존재하는 위치가 K개 주어진다.
    이후 뱀이 몇 초 후에 방향전환을 할지에 대한 명령 L개가 주어진다. 방향전환은 이동이 끝난 후 전환하고 D는 오른쪽으로 90도 회전, L은 왼쪽으로 90도 회전이다.
    덱을 이용하여 뱀의 움직임을 구현한다.
    다음 칸은 drtX와 drtY를 이용하여 0은 오른쪽, 1은 아래, 2는 왼쪽, 3은 오른쪽과 같이 구현한다.
    다음 칸이 자기자신과 부딪히거나 벽에 부딪히면 i를 출력한 후 바로 종료한다.
    아니라면 덱에 마지막에 뱀의 머리를 추가한 후 움직인 자리에 사과가 없으면 맨 앞의 꼬리를 지운다. 사과를 먹었다면 사과를 없애준다.
    이후 방향전환을 해야 한다면 방향을 저장하고 있는 drtIdx값에 변화를 준다.

---
+++ 3190.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] map = new int[n][n];
+		int k = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int i = 0; i < k; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			map[y - 1][x - 1] = 1;
+		}
+		ArrayList<Direction> arr = new ArrayList<Direction>();
+		int l = Integer.parseInt(br.readLine());
+		for (int i = 0; i < l; i++) {
+			st = new StringTokenizer(br.readLine());
+			int second = Integer.parseInt(st.nextToken());
+			String direction = st.nextToken();
+			arr.add(new Direction(second, direction));
+		}
+		int[] drtX = { 1, 0, -1, 0 };
+		int[] drtY = { 0, 1, 0, -1 };
+		int drtIdx = 0;
+		Deque<Point> snake = new ArrayDeque<Point>();
+		snake.add(new Point(0, 0));
+		for (int i = 1;; i++) {
+			Point next = new Point(snake.getLast().x + drtX[drtIdx], snake.getLast().y + drtY[drtIdx]);
+			boolean aroundCheck = (next.x < 0 || next.x >= n) || (next.y < 0 || next.y >= n);
+			if (ContainPoint(snake.iterator(), next) || aroundCheck) {
+				System.out.println(i);
+				return;
+			} else {
+				snake.addLast(next);
+				if (map[next.x][next.y] != 1)
+					snake.removeFirst();
+				else
+					map[next.x][next.y] = 0;
+			}
+			if (arr.size() != 0 && i == arr.get(0).second) {
+				Direction cur = arr.remove(0);
+				if (cur.direction.equals("D"))
+					drtIdx = ++drtIdx % 4;
+				else {
+					if (drtIdx == 0)
+						drtIdx = 3;
+					else
+						drtIdx--;
+				}
+			}
+		}
+	}
+
+	public static boolean ContainPoint(Iterator<Point> it, Point p) {
+		while (it.hasNext()) {
+			Point cur = it.next();
+			if (cur.x == p.x && cur.y == p.y)
+				return true;
+		}
+		return false;
+	}
+}
+
+class Point {
+	int x;
+	int y;
+
+	public Point(int _x, int _y) {
+		this.x = _x;
+		this.y = _y;
+	}
+}
+
+class Direction {
+	int second;
+	String direction;
+
+	public Direction(int _second, String _direction) {
+		this.second = _second;
+		this.direction = _direction;
+	}
+}
