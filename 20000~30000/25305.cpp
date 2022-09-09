6e52537618513b18ccf35d8f5cf707fe1ded0d08
rave189 <rave189@naver.com>
2022-09-09 14:03:13 +0900

    커트라인 추가

---
+++ 20000~30000/25305.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n, greater<>());
+    cout << arr[k-1];
+}
